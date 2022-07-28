e31d0b645effe3c12db787f56b9b32b44f22b682
Dorotea Corelj <dorotea.corelj@gmail.com>
2022-07-28 19:37:34 +0200

    card design
    added tailwind to card

--- my-chat/src/Components/UI/Card.js
+++ my-chat/src/Components/UI/Card.js
@@ -3,7 +3,7 @@ import styles from './Card.module.css';
 
 const Card = (props) => {
     return (
-        <div className={`${styles.cardContainer} ${props.className}`}>
+        <div className={`${styles.cardContainer} ${props.className} rounded-lg`}>
             {props.children}
         </div>
     )
